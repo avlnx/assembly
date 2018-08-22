2068f36fc3d8ee6fd2ac6a07cf9540e853f2832c
Thyago F. da Silva <thferreira@gmail.com>
2018-08-22 20:08:40 -0300

    Adds read to MuralItem

--- lib/tabs/mural_tab.dart
+++ lib/tabs/mural_tab.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 class MuralItem {
   final String title;
   final String body;
+  final bool read;
 
-  MuralItem(this.title, this.body);
+  MuralItem(this.title, this.body, this.read);
 }
 
 class MuralTab extends StatefulWidget {
@@ -18,40 +19,28 @@ class _MuralTabState extends State<MuralTab> {
   @override
   Widget build(BuildContext context) {
     final items = List<MuralItem>.generate(
-        31, (i) => MuralItem("Edital #${i+1}", "Texto do edital #${i+1}"));
-//    _posts.addAll(items);
+        31, (i) => MuralItem("Edital #${i+1}", "Texto do edital #${i+1}", i % 2 == 0));
 
-    return Column(
-      children: <Widget>[
-        Flexible(
-          fit: FlexFit.loose,
-          child: ListView.builder(
-            itemCount: items.length,
-            itemBuilder: (context, index) {
-              int numOfItemsPerFetch = 10;
+    // Sort items by read status
+    items.sort((a, b) => a.read.compareTo(b.read));
 
-              if (index >= _posts.length) {
-                // we reached the end of the list take 10 more
-                _posts.addAll(items.take(numOfItemsPerFetch));
-                // get rid of the items already added to the _posts array
-                items.length >= numOfItemsPerFetch
-                    ? items.removeRange(0, numOfItemsPerFetch)
-                    : items.clear();
-              }
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        int numOfItemsPerFetch = 10;
 
-              return _buildPostRow(_posts[index]);
-            },
-            padding: EdgeInsets.all(20.0),
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
-          child: RaisedButton(
-            child: Text('Carregar posts já visualizados'),
-            onPressed: () => {},
-          ),
-        ),
-      ],
+        if (index >= _posts.length) {
+          // we reached the end of the list take 10 more
+          _posts.addAll(items.take(numOfItemsPerFetch));
+          // get rid of the items already added to the _posts array
+          items.length >= numOfItemsPerFetch
+              ? items.removeRange(0, numOfItemsPerFetch)
+              : items.clear();
+        }
+
+        return _buildPostRow(_posts[index]);
+      },
+      padding: EdgeInsets.all(20.0),
     );
   }
 
@@ -60,13 +49,13 @@ class _MuralTabState extends State<MuralTab> {
       padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
       child: Card(
         child: ListTile(
-          title: Center(
-              child: Text(
+          leading: Icon(Icons.chat_bubble, color: Colors.deepOrange,),
+          title: Text(
             item.title,
             style: TextStyle(
               fontSize: 24.0,
             ),
-          )),
+          ),
           subtitle: Text(
               item.body.length > 50 ? item.body.substring(0, 50) : item.body),
         ),
